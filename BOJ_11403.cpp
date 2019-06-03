f5f7cad95adcaa1380368cb66d1865d326d32d67
kkw <kkw564@naver.com>
2019-06-03 16:55:40 +0900

    DFS

--- BOJ_11403.cpp
+++ BOJ_11403.cpp
@@ -3,14 +3,13 @@
 int arr[102][102], dist[102][102];
 int n;
 
-void dfs(int start, int here, int canGo)
+void dfs(int start, int here)
 {
-	if (canGo)
-		dist[start][here] = 1;
-
 	for (int i = 0; i < n; i++)
-		if (arr[here][i] == 1 && dist[start][i] == 0)
-			dfs(start, i, 1);
+		if (arr[here][i] == 1 && dist[start][i] == 0) {
+			dist[start][i] = 1;
+			dfs(start, i);
+		}
 }
 
 int main()
@@ -20,7 +19,7 @@ int main()
 		for (int j = 0; j < n; j++)
 			scanf("%d", &arr[i][j]);
 	for (int i = 0; i < n; i++)
-		dfs(i, i, 0);
+		dfs(i, i);
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
